3c3f9d3f906eca2047266ee0bc4ba0e858c8ac6e
Your Name <you@example.com>
2023-03-01 20:45:15 +0200

    first commit

--- app-react/src/App.js
+++ app-react/src/App.js
@@ -2,48 +2,54 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
+      stringg: "hello",
       result2: [],
       searchQuery: '',
       isLoading: true
-    }
+    };
   }
-  
-  async componentDidMount() {
-    try {
-      const result = await fetch("https://api.openweathermap.org/data/2.5/forecast?q=kigali&appid=1383c1ef39cccbbc106d269850cfc481")
-      const result2 = await result.json()
-      this.setState({ result2, isLoading: false })
-    } catch (error) {
-      console.error(error);
-      this.setState({ isLoading: false })
-    }
+
+  componentDidMount() {
+    fetch("https://api.openweathermap.org/data/2.5/forecast?q=kigali&appid=1383c1ef39cccbbc106d269850cfc481")
+      .then(result => result.json())
+      .then(result2 => {
+        this.setState({ result2, isLoading: false });
+      })
+      .catch(error => {
+        console.error(error);
+        this.setState({ isLoading: false });
+      });
   }
-  
+
   handleInputChange = (event) => {
     const searchQuery = event.target.value;
     this.setState({ searchQuery });
-  }
-  
+    // console.log(searchQuery);
+  };
+
   render() {
     const { result2, searchQuery, isLoading } = this.state;
     const filteredList = result2.list && result2.list.filter(res =>
       res.weather[0].description.toLowerCase().includes(searchQuery.toLowerCase())
     );
-    
+
     return (
       <div className="App">
-        <input onChange={this.handleInputChange} type='text'></input>
+        <nav>
+        <input onChange={this.handleInputChange} type='text'/>
+        </nav>
+        <section>
         {isLoading ? (
-          <div className="loading-spinner">Loading...</div>
+          <div className="loading">Loading...</div>
         ) : (
-          filteredList && filteredList.map(res =>
-            <p className='prag' key={res.dt}>{res.weather[0].description}</p>
+          filteredList.map(res =>
+            <h1 className='prag' key={res.dt} >{res.weather[0].description}</h1>
           )
         )}
-        {/* <button className="btn" onClick={this.message}>Click Me</button>  */}
+        </section>
       </div>
     );
   }
